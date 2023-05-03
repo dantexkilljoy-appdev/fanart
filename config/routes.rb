a16cdbaf5700fd9aafa0a07c430c6a569c9f3b24
Dustin Doyle <126806607+Dantexkilljoy@users.noreply.github.com>
2023-05-03 20:19:32 +0000

    add home

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,20 @@
 Rails.application.routes.draw do
-
-
+  get("/", { controller: "fan_arts", action: "index" })
 
   # Routes for the Like resource:
 
   # CREATE
   post("/insert_like", { :controller => "likes", :action => "create" })
-          
+
   # READ
   get("/likes", { :controller => "likes", :action => "index" })
-  
+
   get("/likes/:path_id", { :controller => "likes", :action => "show" })
-  
+
   # UPDATE
-  
+
   post("/modify_like/:path_id", { :controller => "likes", :action => "update" })
-  
+
   # DELETE
   get("/delete_like/:path_id", { :controller => "likes", :action => "destroy" })
 
@@ -25,16 +24,16 @@ Rails.application.routes.draw do
 
   # CREATE
   post("/insert_fan_art", { :controller => "fan_arts", :action => "create" })
-          
+
   # READ
   get("/fan_arts", { :controller => "fan_arts", :action => "index" })
-  
+
   get("/fan_arts/:path_id", { :controller => "fan_arts", :action => "show" })
-  
+
   # UPDATE
-  
+
   post("/modify_fan_art/:path_id", { :controller => "fan_arts", :action => "update" })
-  
+
   # DELETE
   get("/delete_fan_art/:path_id", { :controller => "fan_arts", :action => "destroy" })
 
@@ -44,16 +43,16 @@ Rails.application.routes.draw do
 
   # CREATE
   post("/insert_photo", { :controller => "photos", :action => "create" })
-          
+
   # READ
   get("/photos", { :controller => "photos", :action => "index" })
-  
+
   get("/photos/:path_id", { :controller => "photos", :action => "show" })
-  
+
   # UPDATE
-  
+
   post("/modify_photo/:path_id", { :controller => "photos", :action => "update" })
-  
+
   # DELETE
   get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy" })
 
@@ -62,15 +61,15 @@ Rails.application.routes.draw do
   # Routes for the User account:
 
   # SIGN UP FORM
-  get("/user_sign_up", { :controller => "user_authentication", :action => "sign_up_form" })        
+  get("/user_sign_up", { :controller => "user_authentication", :action => "sign_up_form" })
   # CREATE RECORD
-  post("/insert_user", { :controller => "user_authentication", :action => "create"  })
-      
-  # EDIT PROFILE FORM        
-  get("/edit_user_profile", { :controller => "user_authentication", :action => "edit_profile_form" })       
+  post("/insert_user", { :controller => "user_authentication", :action => "create" })
+
+  # EDIT PROFILE FORM
+  get("/edit_user_profile", { :controller => "user_authentication", :action => "edit_profile_form" })
   # UPDATE RECORD
   post("/modify_user", { :controller => "user_authentication", :action => "update" })
-  
+
   # DELETE RECORD
   get("/cancel_user_account", { :controller => "user_authentication", :action => "destroy" })
 
@@ -80,10 +79,10 @@ Rails.application.routes.draw do
   get("/user_sign_in", { :controller => "user_authentication", :action => "sign_in_form" })
   # AUTHENTICATE AND STORE COOKIE
   post("/user_verify_credentials", { :controller => "user_authentication", :action => "create_cookie" })
-  
-  # SIGN OUT        
+
+  # SIGN OUT
   get("/user_sign_out", { :controller => "user_authentication", :action => "destroy_cookies" })
-             
+
   #------------------------------
 
 end
